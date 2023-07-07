08430adbed58389cc24b99d160eaa7f74b98bbc9
H Lee Masson <62517738+haemyleemasson@users.noreply.github.com>
2023-07-07 16:10:32 +0100

    Add files via upload

---
+++ erp.m
@@ -0,0 +1,87 @@
+clear all; clc; close all;
+figpath='/Users/haemyleemasson/Desktop/EEG_2022/Data/ERP/';
+filename='erp.mat'
+numsub=20;
+% use ft_timelockanalysis to compute the ERPs and visualize
+if isfile([figpath filename])
+    load([figpath filename])
+    startsub=size(timelock_social_array,2)+1;
+else
+    startsub=1;
+end
+for isub = startsub:numsub
+    cd ../Data
+    data = load([num2str(isub) '_data' '.mat']); %load preprocessed data. 
+    social=[1:39];
+    trialsocial=[];
+    for i=1:size(social,2)
+        index=find(data.trialinfo==social(i));
+        trialsocial=[trialsocial; index];
+    end
+    cfg = [];
+    cfg.trials=trialsocial;
+    timelock_social = ft_timelockanalysis(cfg, data);
+    eeg_ploterp(timelock_social, isub, figpath,'social') %1=social, 2=nonsocial, nothing for all
+    timelock_social_array{isub} = timelock_social;
+    
+    nonsocial=[40:75];
+    trialnonsocial=[];
+    for i=1:size(nonsocial,2)
+        index=find(data.trialinfo==nonsocial(i));
+        trialnonsocial=[trialnonsocial; index];
+    end
+    cfg = [];
+    cfg.trials=trialnonsocial;
+    timelock_nonsocial = ft_timelockanalysis(cfg, data);
+    eeg_ploterp(timelock_nonsocial, isub, figpath,'nonsocial') %1=social, 2=nonsocial, nothing for all
+    timelock_nonsocial_array{isub} = timelock_nonsocial;
+    
+    cfg        = [];
+    cfg.layout = 'acticap-64ch-standard2';
+    figure
+    ft_singleplotER(cfg, timelock_social, timelock_nonsocial);
+    legend({'social', 'nonsocial'})
+    
+    cfg = [];
+    timelock_all = ft_timelockanalysis(cfg, data);
+    eeg_ploterp(timelock_all, isub, figpath, 'all') %1=social, 2=nonsocial, nothing for all
+    timelock_all_array{isub} = timelock_all;
+    
+    cfg = [];
+    cfg.operation = 'subtract';
+    cfg.parameter = 'avg';
+    difference = ft_math(cfg, timelock_social, timelock_nonsocial);
+    eeg_ploterp(difference, isub, figpath, 'diff')
+    timelock_difference_array{isub} = difference;
+end
+%% plot & save grand average topography and global field power
+cfg = [];
+subtimelock_social = ft_timelockgrandaverage(cfg,timelock_social_array{:});
+%save(fullfile(outpath,'avg_erp.mat'),'subtimelock_social','timelock_social_array')
+eeg_ploterp(subtimelock_social,'avg',figpath, 'group_social');
+
+cfg = [];
+subtimelock_nonsocial = ft_timelockgrandaverage(cfg,timelock_nonsocial_array{:});
+%save(fullfile(outpath,'avg_erp.mat'),'subtimelock','timelock_array')
+eeg_ploterp(subtimelock_nonsocial,'avg',figpath, 'group_nonsocial');
+
+cfg = [];
+subtimelock_all = ft_timelockgrandaverage(cfg,timelock_all_array{:});
+%save(fullfile(outpath,'avg_erp.mat'),'subtimelock','timelock_array')
+eeg_ploterp(subtimelock_all,'avg',figpath, 'group_all');
+
+cfg = [];
+subtimelock_diff = ft_timelockgrandaverage(cfg,timelock_difference_array{:});
+%save(fullfile(outpath,'avg_erp.mat'),'subtimelock','timelock_array')
+eeg_ploterp(subtimelock_diff,'avg',figpath, 'group_diff');
+
+cfg        = [];
+cfg.layout = 'acticap-64ch-standard2';
+figure
+ft_singleplotER(cfg, subtimelock_social, subtimelock_nonsocial);
+legend({'social', 'nonsocial'})
+
+outpath=figpath;
+outfile='erp.mat'
+save(fullfile(outpath,outfile),'-v7.3','timelock_all_array','timelock_social_array','timelock_nonsocial_array','timelock_difference_array');
+
---
+++ erp_statistics.m
@@ -0,0 +1,65 @@
+% define the parameters for the statistical comparison
+clear all; clc; close all;
+figpath='/Users/haemyleemasson/Desktop/EEG_2022/Data/ERP/';
+load erp %run erp.m code first
+j=2% j=1 for 200 ms average, j=2 for 100 ms average
+for i=0:5*j+1
+cfg = [];
+k=0.2*i/j;
+cfg.latency     = [-0.2+k 0+k];
+cfg.avgovertime = 'yes';
+cfg.parameter   = 'avg';
+cfg.method      = 'analytic';
+cfg.statistic   = 'ft_statfun_depsamplesT';
+cfg.alpha       = 0.05;
+cfg.correctm    = 'bonferroni';
+
+Nsub = 20;
+cfg.design(1,1:2*Nsub)  = [ones(1,Nsub) 2*ones(1,Nsub)];
+cfg.design(2,1:2*Nsub)  = [1:Nsub 1:Nsub];
+cfg.ivar                = 1; % the 1st row in cfg.design contains the independent variable
+cfg.uvar                = 2; % the 2nd row in cfg.design contains the subject number
+ 
+stat{i+1} = ft_timelockstatistics(cfg,timelock_social_array{:},timelock_nonsocial_array{:});
+mask{i+1}=stat{i+1}.mask;
+size(find(stat{i+1}.mask==1),1)
+end
+
+cfg = [];
+subtimelock_diff = ft_timelockgrandaverage(cfg,timelock_difference_array{:});
+
+%toi = [-0.2 0 0.2 0.4 0.6 0.8 1];
+toi = [-0.2:0.1:1];
+zmin = -3;
+zmax = 3;
+
+% make the plot
+for i=1:12
+cfg = [];
+cfg.commentpos = 'title';
+cfg.layout = 'acticap-64ch-standard2';
+cfg.xlim = toi(i):0.2/j:toi(i+1);
+cfg.avgovertime = 'yes';
+cfg.zlim = [zmin zmax];
+cfg.colormap='parula';
+cfg.comment = 'xlim';
+cfg.commentpos = 'title';
+cfg.highlight = 'labels'; 
+cfg.highlightfontsize=16;
+cfg.highlightchannel = find(mask{i});
+ft_topoplotER(cfg, subtimelock_diff);
+%add colorbar
+pos = get(gca,'Position');
+title(['Time = [', num2str(cfg.xlim(1)*1000),' ms - ', num2str(cfg.xlim(2)*1000),' ms]'], 'FontSize',20);
+colorbar('FontSize',16);
+print(gcf,'-dpng','-r300',fullfile(figpath, ['avg_erp_gfp_group_diff_tstats_bonferroni_' num2str(i)]));
+fprintf('%f - \n', cfg.xlim);
+fprintf('Maximum %s \n', char(stat{1,i}.label(find(stat{1,i}.stat==max(stat{1,i}.stat)))));
+fprintf('minimum %s \n', char(stat{1,i}.label(find(stat{1,i}.stat==min(stat{1,i}.stat)))));
+end
+
+
+
+
+
+
